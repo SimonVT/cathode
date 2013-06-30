178b3bc8a1895bd1e1e3054837c8f5e495bd8d8e
Simon Vig Therkildsen <simonvt@gmail.com>
2013-06-30 15:40:46 +0200

    Use last activity to sync.

--- trakt/src/main/java/net/simonvt/trakt/sync/task/SyncTask.java
+++ trakt/src/main/java/net/simonvt/trakt/sync/task/SyncTask.java
@@ -1,7 +1,10 @@
 package net.simonvt.trakt.sync.task;
 
-import net.simonvt.trakt.api.service.ShowsService;
+import retrofit.RetrofitError;
+
+import net.simonvt.trakt.api.entity.LastActivity;
 import net.simonvt.trakt.api.service.UserService;
+import net.simonvt.trakt.provider.ActivityWrapper;
 import net.simonvt.trakt.util.LogWrapper;
 
 import javax.inject.Inject;
@@ -12,15 +15,55 @@ public class SyncTask extends TraktTask {
 
     @Inject transient UserService mUserService;
 
-    @Inject transient ShowsService mShowsService;
-
     @Override
     protected void doTask() {
         LogWrapper.v(TAG, "[doTask]");
 
-        queueTask(new SyncShowsTask());
-        queueTask(new SyncMoviesTask());
+        try {
+            queueTask(new SyncUpdatedShows());
+            queueTask(new SyncUpdatedMovies());
 
-        postOnSuccess();
+            LastActivity lastActivity = mUserService.lastActivity();
+
+            LastActivity.ActivityItem episodeActivity = lastActivity.getEpisode();
+            Long lastWatched = episodeActivity.getWatched();
+            Long lastCollected = episodeActivity.getCollection();
+
+            if (lastWatched == null || lastWatched == 0 || lastCollected == null || lastCollected == 0) {
+                queueTask(new SyncShowsTask());
+            } else {
+                boolean needsUpdate =
+                        ActivityWrapper.episodeWatchedNeedsUpdate(mService.getContentResolver(), lastWatched);
+                if (needsUpdate) {
+                    queueTask(new SyncWatchedStatusTask());
+                }
+                needsUpdate = ActivityWrapper.episodeCollectedNeedsUpdate(mService.getContentResolver(), lastWatched);
+                if (needsUpdate) {
+                    queueTask(new SyncShowsCollectionTask());
+                }
+            }
+
+            LastActivity.ActivityItem movieActivity = lastActivity.getMovie();
+            lastWatched = movieActivity.getWatched();
+            lastCollected = movieActivity.getCollection();
+
+            if (lastWatched == null || lastWatched == 0 || lastCollected == null || lastCollected == 0) {
+                queueTask(new SyncMoviesTask());
+            } else {
+                boolean needsUpdate =
+                        ActivityWrapper.movieWatchedNeedsUpdate(mService.getContentResolver(), lastWatched);
+                needsUpdate |= ActivityWrapper.movieCollectedNeedsUpdate(mService.getContentResolver(), lastWatched);
+                if (needsUpdate) {
+                    queueTask(new SyncMoviesTask());
+                }
+            }
+
+            ActivityWrapper.update(mService.getContentResolver(), lastActivity);
+
+            postOnSuccess();
+        } catch (RetrofitError e) {
+            e.printStackTrace();
+            postOnFailure();
+        }
     }
 }
