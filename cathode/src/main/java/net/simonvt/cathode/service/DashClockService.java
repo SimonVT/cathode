791ed54f57c838780a09adb4732a2ed61ef99161
Simon Vig Therkildsen <simonvt@gmail.com>
2013-10-06 19:44:37 +0200

    Only display shows in dashclock that are watched or in watchlist.

--- cathode/src/main/java/net/simonvt/cathode/service/DashClockService.java
+++ cathode/src/main/java/net/simonvt/cathode/service/DashClockService.java
@@ -4,15 +4,26 @@ import android.database.Cursor;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 import net.simonvt.cathode.provider.CathodeContract;
+import net.simonvt.cathode.provider.CathodeDatabase;
 import net.simonvt.cathode.util.DateUtils;
 
 public class DashClockService extends DashClockExtension {
 
   @Override
   protected void onUpdateData(int reason) {
-
     Cursor c = getContentResolver().query(CathodeContract.Episodes.CONTENT_URI, null,
-        CathodeContract.Episodes.FIRST_AIRED + ">? AND " + CathodeContract.Episodes.WATCHED + "=0",
+        CathodeContract.Episodes.FIRST_AIRED + ">? AND "
+        + CathodeContract.Episodes.WATCHED + "=0"
+        + " AND ((SELECT " + CathodeDatabase.Tables.SHOWS + "." + CathodeContract.Shows.WATCHED_COUNT
+        + " FROM " + CathodeDatabase.Tables.SHOWS + " WHERE "
+        + CathodeDatabase.Tables.SHOWS + "." + CathodeContract.Shows._ID + "="
+        + CathodeDatabase.Tables.EPISODES + "." + CathodeContract.Episodes.SHOW_ID
+        + ")>0 OR " + CathodeContract.Episodes.IN_WATCHLIST + "=1 OR "
+        + "(SELECT " + CathodeDatabase.Tables.SHOWS + "." + CathodeContract.Shows.IN_WATCHLIST
+        + " FROM " + CathodeDatabase.Tables.SHOWS + " WHERE "
+        + CathodeDatabase.Tables.SHOWS + "." + CathodeContract.Shows._ID + "="
+        + CathodeDatabase.Tables.EPISODES + "." + CathodeContract.Episodes.SHOW_ID
+        + ")=1)",
         new String[] {
             String.valueOf(System.currentTimeMillis()),
         }, CathodeContract.Episodes.FIRST_AIRED + " ASC LIMIT 1");
