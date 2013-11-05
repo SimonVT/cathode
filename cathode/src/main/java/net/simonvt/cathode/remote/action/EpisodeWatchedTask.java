c4780ce303861316a06b47f36d37784908b0d0a1
Simon Vig Therkildsen <simonvt@gmail.com>
2013-11-05 19:23:10 +0100

    Checkstyle fix.

--- cathode/src/main/java/net/simonvt/cathode/remote/action/EpisodeWatchedTask.java
+++ cathode/src/main/java/net/simonvt/cathode/remote/action/EpisodeWatchedTask.java
@@ -6,12 +6,9 @@ import net.simonvt.cathode.api.entity.Response;
 import net.simonvt.cathode.api.service.ShowService;
 import net.simonvt.cathode.provider.EpisodeWrapper;
 import net.simonvt.cathode.remote.TraktTask;
-import net.simonvt.cathode.util.LogWrapper;
 import retrofit.RetrofitError;
 
 public class EpisodeWatchedTask extends TraktTask {
-
-  private static final String TAG = "EpisodeWatchedTask";
 
   @Inject transient ShowService showService;
 
@@ -24,10 +21,7 @@ public class EpisodeWatchedTask extends TraktTask {
   private final boolean watched;
 
   public EpisodeWatchedTask(int tvdbId, int season, int episode, boolean watched) {
-    if (tvdbId == 0) {
-      // TODO
-      throw new IllegalArgumentException("tvdb is 0");
-    }
+    if (tvdbId == 0) throw new IllegalArgumentException("tvdb is 0");
     this.tvdbId = tvdbId;
     this.season = season;
     this.episode = episode;
@@ -38,11 +32,9 @@ public class EpisodeWatchedTask extends TraktTask {
   protected void doTask() {
     try {
       if (watched) {
-        Response response =
-            showService.episodeSeen(new ShowEpisodeBody(tvdbId, season, episode));
+        Response response = showService.episodeSeen(new ShowEpisodeBody(tvdbId, season, episode));
       } else {
-        Response response =
-            showService.episodeUnseen(new ShowEpisodeBody(tvdbId, season, episode));
+        Response response = showService.episodeUnseen(new ShowEpisodeBody(tvdbId, season, episode));
       }
 
       EpisodeWrapper.setWatched(service.getContentResolver(), tvdbId, season, episode, watched);
