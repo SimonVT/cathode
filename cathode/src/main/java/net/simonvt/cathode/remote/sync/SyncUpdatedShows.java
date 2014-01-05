b7d22a41b82595a49e8bac007e7f35003228a24d
Simon Vig Therkildsen <simonvt@gmail.com>
2014-01-05 16:38:33 +0100

    Remove unnecessary sync.

--- cathode/src/main/java/net/simonvt/cathode/remote/sync/SyncUpdatedShows.java
+++ cathode/src/main/java/net/simonvt/cathode/remote/sync/SyncUpdatedShows.java
@@ -19,9 +19,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import java.util.List;
 import javax.inject.Inject;
-import net.simonvt.cathode.api.entity.ServerTime;
 import net.simonvt.cathode.api.entity.UpdatedShows;
-import net.simonvt.cathode.api.service.ServerService;
 import net.simonvt.cathode.api.service.ShowsService;
 import net.simonvt.cathode.provider.ShowWrapper;
 import net.simonvt.cathode.remote.TraktTask;
@@ -31,38 +29,27 @@ public class SyncUpdatedShows extends TraktTask {
 
   @Inject transient ShowsService showsService;
 
-  @Inject transient ServerService serverService;
-
   @Override protected void doTask() {
     SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(service);
     final long showsLastUpdated = settings.getLong(Settings.SHOWS_LAST_UPDATED, 0);
 
-    long current;
+    UpdatedShows updatedShows = showsService.updated(showsLastUpdated);
+    long current = updatedShows.getTimestamps().getCurrent();
 
-    if (showsLastUpdated > 0) {
-      UpdatedShows updatedShows = showsService.updated(showsLastUpdated);
-      current = updatedShows.getTimestamps().getCurrent();
-
-      List<UpdatedShows.ShowTimestamp> timestamps = updatedShows.getShows();
-      for (UpdatedShows.ShowTimestamp timestamp : timestamps) {
-        final int tvdbId = timestamp.getTvdbId();
-        final boolean exists = ShowWrapper.exists(service.getContentResolver(), tvdbId);
-        if (exists) {
-          final boolean needsUpdate = ShowWrapper.needsUpdate(service.getContentResolver(), tvdbId,
-              timestamp.getLastUpdated());
-          if (needsUpdate) {
-            queueTask(new SyncShowTask(tvdbId));
-          }
+    List<UpdatedShows.ShowTimestamp> timestamps = updatedShows.getShows();
+    for (UpdatedShows.ShowTimestamp timestamp : timestamps) {
+      final int tvdbId = timestamp.getTvdbId();
+      final boolean exists = ShowWrapper.exists(service.getContentResolver(), tvdbId);
+      if (exists) {
+        final boolean needsUpdate = ShowWrapper.needsUpdate(service.getContentResolver(), tvdbId,
+            timestamp.getLastUpdated());
+        if (needsUpdate) {
+          queueTask(new SyncShowTask(tvdbId));
         }
       }
-    } else {
-      queueTask(new SyncShowsTask());
-      ServerTime time = serverService.time();
-      current = time.getTimestamp();
     }
 
     settings.edit().putLong(Settings.SHOWS_LAST_UPDATED, current).apply();
-
     postOnSuccess();
   }
 }
